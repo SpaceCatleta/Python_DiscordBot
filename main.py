9a8c26163dc147f5f74449081d97a14e9c3eed54
Фёдор <fedo970@gmail.com>
2020-09-12 01:03:21 +0300

    v0.2.0b теперь бот подсчитывает количество напечатанных пользователем символов, но нигде не записывает, поэтому счётчик сбрасывается каждую сессию

--- main.py
+++ main.py
@@ -5,15 +5,53 @@ import mycommands
 
 # Так как мы указали префикс в settings, обращаемся к словарю с ключом prefix.
 bot = commands.Bot(command_prefix=settings['prefix'])
+guild: discord.Guild
 UserKeys = []
 UserCounter = []
 
 
-# Не передаём аргумент pass_context, так как он был нужен в старых версиях.
-# ctx <Context> - контекст комманды
+@bot.event
+async def on_ready():
+    global guild, UserKeys, UserCounter
+    guild = bot.get_guild(settings['home_guild_id'])
+    print(len(guild.members))
+    for user in guild.members:
+        UserKeys.append(user.id)
+        UserCounter.append(0)
+    print('loading finished')
+
+
+
+@bot.event
+async def on_message(mes: discord.Message):
+    if mes.author.bot:
+        return
+    UserCounter[UserKeys.index(mes.author.id)] += len(mes.content)
+    await bot.process_commands(mes)
+
+
 @bot.command()
-async def tm(ctx: discord.ext.commands.Context):  # Создаём функцию и передаём аргумент ctx.
+async def tm(ctx: discord.ext.commands.Context):
+    await ctx.message.delete()
     await mycommands.hello(ctx)
+
+
+@bot.command()
+async def stats(ctx: discord.ext.commands.Context):
+    await ctx.message.delete()
+    user: discord.abc.User = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else ctx.author
+    await ctx.send('```Напечатано символов: ' + str(UserCounter[UserKeys.index(user.id)]) + '```')
+
+
+@bot.command()
+async def ourguild(ctx: discord.ext.commands.Context):
+    await ctx.message.delete()
+    global guild
+    answer = 'Название сервера: ' + guild.name + '\n'
+    answer += 'Количество участников: ' + str(guild.member_count) + '\nучастники:\n'
+    for user in guild.members:
+        answer += user.name + ' [' + str(user.nick) + ']\n'
+    await ctx.send('```' + answer + '```')
 
 
 @bot.command()
@@ -28,10 +66,15 @@ async def clientdata(ctx: discord.ext.commands.Context):
     await ctx.send('```' + answer + '```')
 
 
+@bot.command()
+async def serverid(ctx: discord.ext.commands.Context):
+    await ctx.send('```ID сервера: ' + str(ctx.author.guild.id) + '```')
+
+
 # команда завершения работы
 @bot.command()
 async def off(ctx: discord.ext.commands.Context):
-    await ctx.send('[bot was turned off]')
+    await ctx.send('```[bot offline]```')
     exit()
 
 
@@ -42,17 +85,6 @@ async def bomb(ctx: discord.ext.commands.Context):
         link = ctx.message.mentions[0]
         for i in range(0, 10):
             await ctx.send(link.mention)
-
-
-# событие при поступлении сообщения в чат
-# 'mes' <Message> - отправленное сообщение
-@bot.event
-async def on_message(mes: discord.Message):
-    if mes.author.bot:
-        return
-    # await mlen: int = len(mes.content)
-    # await mes.channel.send(mes.author.mention)
-    await bot.process_commands(mes)
 
 
 # Обращаемся к словарю settings с ключом token, для получения токена
